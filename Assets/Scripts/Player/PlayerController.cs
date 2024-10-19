e16853eb8ae69ec4936ea445f5b0862d7015a13f
Alex Kong <alexkong07@gmail.com>
2024-10-19 02:11:25 -0500

    Added basic horizontal movement and jumping
    - Added basic horizontal movement
    - Added basic jumping (groundcheck, layers, etc.)

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement")]
     public float moveSpeed;
+    float horizontalMovement;
 
-    Vector2 movement;
+    [Header("Jumping")]
+    public float jumpPower = 10f;
+
+    [SerializeField] Transform groundCheck;
+    CircleCollider2D groundCheckCollider;
+    [SerializeField] LayerMask groundLayer;
 
     Rigidbody2D rb;
     Animator animator;
@@ -14,11 +22,32 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        groundCheckCollider = groundCheck.GetComponent<CircleCollider2D>();
     }
 
     private void Update()
     {
-        movement.Set(PlayerInputManager.movement.x, movement.y);
-        rb.velocity = movement * moveSpeed;
+        rb.velocity = new Vector2(horizontalMovement * moveSpeed, rb.velocity.y);
+        //Debug.Log("Grounded?: " + IsGrounded());
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontalMovement = context.ReadValue<Vector2>().x;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        //Debug.Log("Jump key pressed!");
+        if (context.performed && IsGrounded())
+        {
+            //Debug.Log("Jumping");
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, groundCheckCollider.radius, groundLayer);
     }
 }
--- Assets/Scripts/Player/PlayerInputManager.cs
+++ Assets/Scripts/Player/PlayerInputManager.cs
@@ -1,24 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[Obsolete("Class currently unused.")]
 public class PlayerInputManager : MonoBehaviour
 {
     public static Vector2 movement;
 
     private PlayerInput playerInput;
     private InputAction moveAction;
+    private InputAction jumpAction;
 
     private void Awake()
     {
         playerInput = GetComponent<PlayerInput>();
 
         moveAction = playerInput.actions["Move"];
+        jumpAction = playerInput.actions["Jump"];
     }
 
     private void Update()
     {
         movement = moveAction.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        //Debug.Log("Jump Button pressed?" + jumpAction);
     }
 }
